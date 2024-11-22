848078343516293398cadb1b462003da3d3b4c80
IceKungzz <147721215+IceKungzz@users.noreply.github.com>
2024-11-22 20:59:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ICE kub.
+          Edit <code>src/App.js</code> and save to reload ICE kubbbbbbbbbbbbbbbbb.
         </p>
         <a
           className="App-link"
